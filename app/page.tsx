61bb40e41ba2bebac51a4497d6382bcda2510c99
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-15 20:11:33 +0200

    chore: better colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <p>Nicolas Ghyselincks</p>
-    </div>
-  );
+  return <div></div>;
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -38,94 +38,89 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
 const Header = (props) => {
   const { path } = props;
   return (
-    <Box
+    <Flex
+      as="header"
       position="fixed"
-      as="nav"
       w="100%"
-      bg={useColorModeValue("#ffffff40", "#20202380")}
+      bg={useColorModeValue("#F1E7DB", "#202022")}
       css={{ backdropFilter: "blur(10px)" }}
       zIndex={2}
+      align="center"
+      justify="space-between"
+      p={2}
       {...props}
     >
-      <Container
-        display="flex"
-        p={2}
-        alignContent="center"
-        justifyContent="space-between"
-      >
-        <Flex align="center" mr={5}>
-          <Heading
-            as="h1"
-            size="lg"
-            letterSpacing={"tighter"}
-            color={useColorModeValue("gray.800", "whiteAlpha.900")}
-          >
-            Nicolas GHS
-          </Heading>
-        </Flex>
-
-        <Stack
-          direction={{ base: "column", md: "row" }}
-          display={{ base: "none", md: "flex" }}
-          width={{ base: "full", md: "auto" }}
-          alignItems="center"
-          flexGrow={1}
-          mt={{ base: 4, md: 0 }}
+      <Flex align="center" mr={5}>
+        <Heading
+          as="h1"
+          size="lg"
+          letterSpacing={"tighter"}
+          color={useColorModeValue("gray.800", "whiteAlpha.900")}
         >
-          <LinkItem href="/works" path={path} target="_self">
-            Projects
-          </LinkItem>
-          <LinkItem
-            href="https://github.com/NicolasGHS/portfolio.git"
-            path={path}
-            target="_blank"
-            display="inline-flex"
-            alignItems="center"
-            style={{ gap: 4 }}
-            pl={2}
-          >
-            <IoLogoGithub />
-            Source
-          </LinkItem>
-        </Stack>
-        <Box flex={1} alignItems="right">
-          <ThemeToggleButton />
-          <Box ml={2} display={{ base: "inline-block", md: "none" }}>
-            <Menu.Root>
-              <Menu.Trigger asChild>
-                <Button variant="outline" size="sm">
-                  <HamburgerIcon />
-                </Button>
-              </Menu.Trigger>
-              <Portal>
-                <Menu.Positioner>
-                  <Menu.Content>
-                    <Menu.Item value="projects">
-                      <Link as={NextLink} href="/projects">
-                        Projects
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item value="source">
-                      <Link
-                        as={NextLink}
-                        href="https://github.com/NicolasGHS/portfolio.git"
-                        target="_blank"
-                        display="inline-flex"
-                        alignItems="center"
-                        style={{ gap: 4 }}
-                      >
-                        <IoLogoGithub />
-                        Source
-                      </Link>
-                    </Menu.Item>
-                  </Menu.Content>
-                </Menu.Positioner>
-              </Portal>
-            </Menu.Root>
-          </Box>
+          Nicolas GHS
+        </Heading>
+      </Flex>
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        display={{ base: "none", md: "flex" }}
+        width={{ base: "full", md: "auto" }}
+        alignItems="center"
+        flexGrow={1}
+        mt={{ base: 4, md: 0 }}
+      >
+        <LinkItem href="/works" path={path} target="_self">
+          Projects
+        </LinkItem>
+        <LinkItem
+          href="https://github.com/NicolasGHS/portfolio.git"
+          path={path}
+          target="_blank"
+          display="inline-flex"
+          alignItems="center"
+          style={{ gap: 4 }}
+          pl={2}
+        >
+          <IoLogoGithub />
+          Source
+        </LinkItem>
+      </Stack>
+      <Flex align="center">
+        <ThemeToggleButton />
+        <Box ml={2} display={{ base: "inline-block", md: "none" }}>
+          <Menu.Root>
+            <Menu.Trigger asChild>
+              <Button variant="outline" size="sm">
+                <HamburgerIcon />
+              </Button>
+            </Menu.Trigger>
+            <Portal>
+              <Menu.Positioner>
+                <Menu.Content>
+                  <Menu.Item value="projects">
+                    <Link as={NextLink} href="/projects">
+                      Projects
+                    </Link>
+                  </Menu.Item>
+                  <Menu.Item value="source">
+                    <Link
+                      as={NextLink}
+                      href="https://github.com/NicolasGHS/portfolio.git"
+                      target="_blank"
+                      display="inline-flex"
+                      alignItems="center"
+                      style={{ gap: 4 }}
+                    >
+                      <IoLogoGithub />
+                      Source
+                    </Link>
+                  </Menu.Item>
+                </Menu.Content>
+              </Menu.Positioner>
+            </Portal>
+          </Menu.Root>
         </Box>
-      </Container>
-    </Box>
+      </Flex>
+    </Flex>
   );
 };
 
