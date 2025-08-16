5c3682984aaa2560a5cf0f46543be55859af0681
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-16 14:52:06 +0200

    chore: show only visible projects

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -2,14 +2,27 @@ import { Box, SimpleGrid } from "@chakra-ui/react";
 import { projects } from "@/data/projects";
 import ProjectCard from "@/components/ProjectCard";
 
+type Project = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+  visibility: boolean;
+  status: string;
+  source: string;
+};
+
 const Work = () => {
+  const projectData: Project[] = projects;
   return (
     <Box>
       <p>What I worked on</p>
       <SimpleGrid columns={{ base: 1, md: 3 }} p={6}>
-        {projects.map((project) => (
-          <ProjectCard key={project.id} {...project} />
-        ))}
+        {projectData.map(
+          (project: Project) =>
+            project.visibility && <ProjectCard key={project.id} {...project} />,
+        )}
       </SimpleGrid>
     </Box>
   );
