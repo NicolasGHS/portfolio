0cc90a315f3066849fb01400f7e7d6e29e3a547e
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-30 09:23:49 +0200

    chore: NavigationItem component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,56 +1,28 @@
 import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuLink,
-} from "@/components/ui/navigation-menu";
-import { Menu } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+	NavigationMenu,
+	NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+import {Menu} from "lucide-react"
+import {Button} from "@/components/ui/button"
+import Link from "next/link"
+import {NavigationItem} from "./NavigationItem"
 
 export const Header = () => {
-  return (
-    <header className="w-full flex justify-around gap-8 items-center py-4 px-6">
-      <Link href="/" className="font-bold text-2xl">
-        Nicolas GHS
-      </Link>
-      <NavigationMenu className="hidden md:block">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link
-                href="/works"
-                className="px-3 py-2 rounded hover:bg-muted transition-colors"
-              >
-                Work
-              </Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link
-                href="/skills"
-                className="px-3 py-2 rounded hover:bg-muted transition-colors"
-              >
-                Skills
-              </Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link
-                href="/skills"
-                className="px-3 py-2 rounded hover:bg-muted transition-colors flex gap-2 items-center"
-              >
-                Source
-              </Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-      <Button size="icon" variant="secondary" className="md:hidden">
-        <Menu />
-      </Button>
-    </header>
-  );
-};
+	return (
+		<header className="w-full flex justify-around gap-8 items-center py-4 px-6">
+			<Link href="/" className="font-bold text-2xl">
+				Nicolas GHS
+			</Link>
+			<NavigationMenu className="hidden md:block">
+				<NavigationMenuList>
+					<NavigationItem name="Works" link="/works" />
+					<NavigationItem name="Skills" link="/skills" />
+					<NavigationItem name="Source" link="/skills" />
+				</NavigationMenuList>
+			</NavigationMenu>
+			<Button size="icon" variant="secondary" className="md:hidden">
+				<Menu />
+			</Button>
+		</header>
+	)
+}
---
+++ app/components/NavigationItem.tsx
@@ -0,0 +1,25 @@
+import {
+	NavigationMenuItem,
+	NavigationMenuLink,
+} from "@radix-ui/react-navigation-menu"
+import Link from "next/link"
+
+type ItemProps = {
+	name: string
+	link: string
+}
+
+export const NavigationItem = ({name, link}: ItemProps) => {
+	return (
+		<NavigationMenuItem>
+			<NavigationMenuLink asChild>
+				<Link
+					href={link}
+					className="px-3 py-2 rounded hover:bg-muted transition-colors"
+				>
+					{name}
+				</Link>
+			</NavigationMenuLink>
+		</NavigationMenuItem>
+	)
+}
