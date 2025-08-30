0351d3a9bde5b03d692a21619fa7ed787975ce5b
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-30 09:44:46 +0200

    chore: background colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	important: true,
+	content: [
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				yellow: "#fcd28d",
+				blue: "#82e6d9",
+			}
+		}
+	}
+}
