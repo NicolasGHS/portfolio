ef33befe642652bc06023b2dbbb69192d3710404
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-30 09:23:39 +0200

    eslint + prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+
+import { defineConfig, globalIgnores } from "eslint/config"
+import unusedImports from "eslint-plugin-unused-imports"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+})
+
+export default defineConfig([
+	globalIgnores([
+		"**/node_modules/*",
+		"**/node_modules/",
+		"graphql/generated/*",
+		".next",
+	]),
+	{
+		extends: compat.extends(
+			"next",
+			"next/core-web-vitals",
+			"plugin:prettier/recommended"
+		),
+
+		plugins: {
+			"unused-imports": unusedImports,
+		},
+
+		rules: {
+			"prettier/prettier": [
+				"error",
+				{
+					singleQuote: false,
+				},
+			],
+
+			"react-hooks/exhaustive-deps": "warn",
+			"no-console": "error",
+			"unused-imports/no-unused-imports-ts": "error",
+		},
+	},
+])
+
