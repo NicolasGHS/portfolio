a1d7c67493e1f9628974a8d2c56c28fb5bd23f4b
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-09-01 17:00:18 +0200

    chore: category in stack

--- data/technology.ts
+++ data/technology.ts
@@ -2,69 +2,86 @@ export const technologies = [
 	{
 		id: 1,
 		name: "React",
+		category: "Frontend",
 	},
 	{
 		id: 2,
 		name: "Golang",
+		category: "Backend",
 	},
 	{
 		id: 3,
 		name: "Tailwind",
+		category: "Frontend",
 	},
 	{
 		id: 4,
 		name: "CraftCMS",
+		category: "CMS",
 	},
 	{
 		id: 5,
 		name: "Shadcn",
+		category: "Frontend",
 	},
 	{
 		id: 6,
 		name: ".NET",
+		category: "Backend",
 	},
 	{
 		id: 7,
 		name: "MongoDB",
+		category: "Database",
 	},
 	{
 		id: 8,
 		name: "SQL",
+		category: "Database",
 	},
 	{
 		id: 9,
 		name: "Wails",
+		category: "Backend",
 	},
 	{
 		id: 10,
 		name: "Sqlite",
+		category: "Database",
 	},
 	{
 		id: 11,
 		name: "React Native",
+		category: "Frontend",
 	},
 	{
 		id: 12,
 		name: "Supabase",
+		category: "CMS",
 	},
 	{
 		id: 13,
 		name: "HTML",
+		category: "Frontend",
 	},
 	{
 		id: 14,
 		name: "CSS",
+		category: "Frontend",
 	},
 	{
 		id: 15,
 		name: "Javascript",
+		category: "Frontend",
 	},
 	{
 		id: 16,
 		name: "Express",
+		category: "Backend",
 	},
 	{
 		id: 17,
 		name: "Hygraph",
+		category: "CMS",
 	},
 ];
--- types/technology.ts
+++ types/technology.ts
@@ -1,4 +1,5 @@
 export type Technology = {
 	id: number;
 	name: string;
+	category: string;
 };
