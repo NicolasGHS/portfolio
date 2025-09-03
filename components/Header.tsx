c102cf5f3aa4e12484aea2f8abd4cc08666fafca
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-09-03 15:25:36 +0200

    chore: dropdown on hamburger

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,15 @@ import {
 	NavigationMenu,
 	NavigationMenuList,
 } from "@/components/ui/navigation-menu";
-import {Menu} from "lucide-react";
+import {Menu, Briefcase, Code, Github} from "lucide-react";
 import {Button} from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+	DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 import Link from "next/link";
 import {NavigationItem} from "./NavigationItem";
 import {ModeToggle} from "@/components/mode-toggle";
@@ -45,9 +52,39 @@ export const Header = () => {
 				</NavigationMenuList>
 			</NavigationMenu>
 			<ModeToggle />
-			<Button size="icon" variant="secondary" className="md:hidden">
-				<Menu />
-			</Button>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button size="icon" variant="secondary" className="md:hidden">
+						<Menu />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align="end" className="w-48">
+					<DropdownMenuItem asChild>
+						<Link href="/works" className="flex items-center">
+							<Briefcase className="mr-2 h-4 w-4" />
+							Works
+						</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem asChild>
+						<Link href="/skills" className="flex items-center">
+							<Code className="mr-2 h-4 w-4" />
+							Skills
+						</Link>
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem asChild>
+						<a 
+							href="https://github.com/NicolasGHS/portfolio.git" 
+							target="_blank" 
+							rel="noopener noreferrer"
+							className="flex items-center"
+						>
+							<Github className="mr-2 h-4 w-4" />
+							Source Code
+						</a>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
 		</header>
 	);
 };
